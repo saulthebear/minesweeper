30e623609b3fbf603834a8bdf5b24f4882a52df7
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-17 11:16:04 -0600

    Add validation for Position

--- lib/position.rb
+++ lib/position.rb
@@ -9,6 +9,25 @@ class Position
     @col = col
   end
 
+  # Has the position's row & col values been properly initialized?
+  # (optional) Are the values in range, meaning between zero and max specified
+  # in the parameters? If no maximum is specified, all integer values are
+  # concidered valid.
+  # @param max_row [Integer] (optional) The maximum value @row can take
+  # @param max_col [Integer] (optional) The maximum value @col can take
+  # @return [true, false]
+  def valid?(max_row = nil, max_col = nil)
+    # Have both row and col been initialized?
+    return false if @row.nil? || @col.nil?
+
+    # Is the row in range?
+    return false unless max_row.nil? || (0..max_row).cover?(@row)
+    # Is the col in range?
+    return false unless max_col.nil? || (0..max_col).cover?(@col)
+
+    true
+  end
+
   def to_s
     "[#{@row},#{@col}]"
   end
@@ -27,6 +46,6 @@ class Position
 
   # Ensure row is printed first
   def inspect
-    "#<Position: #{object_id} @row=#{@row}, @col=#{@col}>"
+    "#<Position: @row=#{@row}, @col=#{@col}>"
   end
 end
