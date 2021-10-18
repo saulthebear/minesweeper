2e6ed4b4b5fc6fd82bf0b1c1f9c7c2209f40b8a5
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-17 21:38:42 -0600

    Add functionality to Game
    Basic game loop and user interaction added

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative 'board'
+require_relative 'position'
 
 # Holds the main game loop
 class Game
@@ -6,14 +7,85 @@ class Game
     @board = Board.new
   end
 
-  def run() end
+  def run
+    display_introduction
+    1.step do |run_number|
+      render(run_number)
+      valid_command = receive_command while valid_command.nil?
+      if valid_command == 1
+        render(run_number)
+        puts "BOOM!\n=== GAME OVER ==="
+        break
+      end
 
-  def render() end
+      if @board.win?
+        render(run_number)
+        puts "\n=== BOARD SOLVED! ==="
+        break
+      end
+    end
+  end
 
-  def receive_command() end
+  def display_introduction
+    system('clear')
+    puts "==== Welcome to Minesweeper! ====\n\n"
+    puts '=== INSTRUCTIONS ==='
+    puts 'Commands are one letter followed by a position, eg.'
+    puts '    r 0,4 => reveal row 0 column 4'
+    puts '    f 5,2 => flag row 5 column 2'
+    puts "\n"
+  end
 
-  def flag(position) end
+  def render(run_number)
+    system('clear') unless run_number.zero?
+    @board.render
+  end
 
-  def reveal(position) end
+  def receive_command
+    puts 'Enter a command:'
+    print '> '
+    begin
+      command, position = process_input(user_input)
+    rescue ArgumentError => error
+      puts "Invalid input: #{error.message}"
+      nil
+    else
+      process_command(command, position)
+    end
+  end
+
+  def process_input(input_string)
+    command, position_string = input_string.split(' ')
+    raise ArgumentError unless command && position_string
+
+    row, col = position_string.split(',').map(&:to_i)
+    position = Position.new(row, col)
+    raise(ArgumentError, 'Position Invalid') unless position.valid?(@board.width - 1, @board.height - 1)
+
+    raise(ArgumentError, "`#{command}` is not a valid command") unless %w[r f].include?(command)
+
+    [command, position]
+  end
+
+  def process_command(command, position)
+    case command
+    when 'r'
+      reveal(position)
+    when 'f'
+      flag(position)
+    end
+  end
+
+  def user_input
+    gets.chomp
+  end
+
+  def reveal(position)
+    @board.reveal(position)
+  end
+
+  def flag(position)
+    @board.flag(position)
+  end
 
 end
